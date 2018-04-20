5f114a818cf591010b491a7a6ea25b9b26a35cfd
Truong-AQ <37512106+Truong-AQ@users.noreply.github.com>
2018-04-20 14:21:32 +0700

    Delete readWord.h

--- readWord.h
+++
@@ -1,6 +0,0 @@
-#ifndef READWORD_H
-#define READWORD_H
-
-void createWordList(vector <Word> &wordList, const string nameFile);//tao danh sach tu tu tep ten la nameFile, doc va luu no vao vector wordList
-
-#endif
